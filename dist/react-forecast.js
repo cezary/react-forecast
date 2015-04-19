8c622ccf26333fc15c58420525c805d68dc1472a
Cezary Wojtkowski <cezary@gmail.com>
2015-04-19 01:34:59 -0700

    clean up package.json, remove webpack and use babel

--- dist/react-forecast.js
+++ dist/react-forecast.js
@@ -1,101 +1,21 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("React"));
-	else if(typeof define === 'function' && define.amd)
-		define(["React"], factory);
-	else if(typeof exports === 'object')
-		exports["Forecast"] = factory(require("React"));
-	else
-		root["Forecast"] = factory(root["React"]);
-})(this, function(__WEBPACK_EXTERNAL_MODULE_2__) {
-return /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
-/******/ 			return installedModules[moduleId].exports;
-/******/
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
+'use strict';
 
-	module.exports = __webpack_require__(1);
+var React = require('react');
 
+module.exports = React.createClass({
+  displayName: 'Forecast',
 
-/***/ },
-/* 1 */
-/***/ function(module, exports, __webpack_require__) {
+  getDefaultProps: function getDefaultProps() {
+    return {
+      height: 245,
+      width: '100%'
+    };
+  },
 
-	var React = __webpack_require__(2);
-	
-	module.exports = React.createClass({
-	  displayName: 'Forecast',
-	
-	  getDefaultProps: function() {
-	    return {
-	      height: 245,
-	      width: '100%'
-	    };
-	  },
-	
-	  render: function() {
-	    var url = "http://forecast.io/embed/#lat=" + this.props.latitude +
-	                "&lon=" + this.props.longitude +
-	                "&name=" + this.props.name || '' +
-	                "&color=" + this.props.color || '' +
-	                "&font=" + this.props.font || '' +
-	                "&units=" + this.props.unit || '';
-	
-	    return (
-	      React.createElement("iframe", {type: "text/html", height: this.props.height, width: this.props.width, frameBorder: "0", src: url})
-	    );
-	  }
-	});
+  render: function render() {
+    var url = 'http://forecast.io/embed/#lat=' + this.props.latitude + '&lon=' + this.props.longitude + '&name=' + this.props.name || '' + '&color=' + this.props.color || '' + '&font=' + this.props.font || '' + '&units=' + this.props.unit || '';
 
-
-/***/ },
-/* 2 */
-/***/ function(module, exports, __webpack_require__) {
-
-	module.exports = __WEBPACK_EXTERNAL_MODULE_2__;
-
-/***/ }
-/******/ ])
+    return React.createElement('iframe', { type: 'text/html', height: this.props.height, width: this.props.width, frameBorder: '0', src: url });
+  }
 });
 
-//# sourceMappingURL=react-forecast.map
