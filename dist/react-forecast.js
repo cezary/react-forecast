77c7c41c1ae5293c7f077e9259e67732e9981527
Po-Cheng Chu <darkbtf@gmail.com>
2017-02-05 20:02:33 +0800

    fix bug for parameters

--- dist/react-forecast.js
+++ dist/react-forecast.js
@@ -1,21 +1,18 @@
-'use strict';
-
 var React = require('react');
 
 module.exports = React.createClass({
   displayName: 'Forecast',
 
-  getDefaultProps: function getDefaultProps() {
+  getDefaultProps: function () {
     return {
       height: 245,
       width: '100%'
     };
   },
 
-  render: function render() {
-    var url = '//forecast.io/embed/#lat=' + this.props.latitude + '&lon=' + this.props.longitude + '&name=' + this.props.name || '' + '&color=' + this.props.color || '' + '&font=' + this.props.font || '' + '&units=' + this.props.unit || '';
+  render: function () {
+    var url = "//forecast.io/embed/#lat=" + this.props.latitude + "&lon=" + this.props.longitude + ("&name=" + this.props.name || '') + ("&color=" + this.props.color || '') + ("&font=" + this.props.font || '') + ("&units=" + this.props.units || '');
 
     return React.createElement('iframe', { type: 'text/html', height: this.props.height, width: this.props.width, frameBorder: '0', src: url });
   }
 });
-
